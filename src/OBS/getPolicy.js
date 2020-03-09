1a9e67d7674228ece3130c0d68a7e7b7a8c26de5
hxz <1725672603@qq.com>
2020-03-09 11:42:57 +0800

    vconsole

--- src/OBS/getPolicy.js
+++
@@ -1,8 +0,0 @@
-const Base64 = require('./Base64.js');
-
-function getPolicyEncode(policy) {
-  const encodedPolicy = Base64.encode(JSON.stringify(policy));
-  return encodedPolicy;
-}
-
-module.exports = getPolicyEncode;
